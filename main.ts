695dea2dd9e9b45bda90765b72d46b16b71f0822
Thomas Henderson <48155717+Hoof60@users.noreply.github.com>
2023-01-23 15:57:20 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let APressed = 0
 basic.forever(function () {
-	
+    if (APressed == 0 && input.buttonIsPressed(Button.B)) {
+        APressed = 1
+        serial.writeLine("t:-1")
+    } else {
+        if (APressed == 1 && !(input.buttonIsPressed(Button.B))) {
+            APressed = 0
+            serial.writeLine("t:0")
+        }
+    }
+    serial.writeLine("h:" + (input.compassHeading()))
 })
