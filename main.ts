f45883664d1d6268ee5dedb6f390b2b321df461d
Thomas Henderson <48155717+Hoof60@users.noreply.github.com>
2023-01-23 16:35:57 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,5 +9,29 @@ basic.forever(function () {
             serial.writeLine("t:0")
         }
     }
-    serial.writeLine("h:" + (input.compassHeading()))
+
+    let pitchComponent = input.rotation(Rotation.Pitch)
+    let rollComponent = input.rotation(Rotation.Roll)
+
+    let a_mag = Math.sqrt(pitchComponent**2 + rollComponent**2)
+
+    
+
+
+    //if (input.rotation(Rotation.Pitch)){
+    if (input.rotation(Rotation.Pitch) > 0){
+        serial.writeLine("h:0")
+    }
+    if (input.rotation(Rotation.Pitch) < 0) {
+        serial.writeLine("h:270")
+    }
+    if (input.rotation(Rotation.Roll) > 0) {
+        serial.writeLine("h:90")
+    }
+    if (input.rotation(Rotation.Roll) < 0) {
+        serial.writeLine("h:270")
+    }
+
+
+    //}
 })
