28dd75c26a7f8c3c1ad432e8573be057e2e7921a
Thomas Henderson <48155717+Hoof60@users.noreply.github.com>
2023-01-24 13:13:01 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,29 @@
+let angle = 0
+let b_mag = 0
+let a_mag = 0
+let rollComponent = 0
+let pitchComponent = 0
 let APressed = 0
+radio.setGroup(24)
 basic.forever(function () {
-    if (APressed == 0 && input.buttonIsPressed(Button.B)) {
-        APressed = 1
-        serial.writeLine("t:-1")
-    } else {
-        if (APressed == 1 && !(input.buttonIsPressed(Button.B))) {
-            APressed = 0
-            serial.writeLine("t:0")
-        }
+    // if (APressed == 0 && input.buttonIsPressed(Button.B)) {
+    // APressed = 1
+    // serial.writeLine("t:-1")
+    // } else {
+    // if (APressed == 1 && !(input.buttonIsPressed(Button.B))) {
+    // APressed = 0
+    // serial.writeLine("t:0")
+    // }
+    // }
+    pitchComponent = 0 - input.rotation(Rotation.Pitch)
+    rollComponent = input.rotation(Rotation.Roll)
+    a_mag = Math.sqrt(pitchComponent ** 2 + rollComponent ** 2)
+    b_mag = 1
+    angle = Math.acos((rollComponent * 0 + pitchComponent * 1) / (a_mag * b_mag))
+    if (rollComponent > 0) {
+        radio.sendString("h:" + angle * 180 / Math.PI)
     }
-
-    let pitchComponent = input.rotation(Rotation.Pitch)
-    let rollComponent = input.rotation(Rotation.Roll)
-
-    let a_mag = Math.sqrt(pitchComponent**2 + rollComponent**2)
-
-    
-
-
-    //if (input.rotation(Rotation.Pitch)){
-    if (input.rotation(Rotation.Pitch) > 0){
-        serial.writeLine("h:0")
+    if (rollComponent < 0) {
+        radio.sendString("h:" + (360 - angle * 180 / Math.PI))
     }
-    if (input.rotation(Rotation.Pitch) < 0) {
-        serial.writeLine("h:270")
-    }
-    if (input.rotation(Rotation.Roll) > 0) {
-        serial.writeLine("h:90")
-    }
-    if (input.rotation(Rotation.Roll) < 0) {
-        serial.writeLine("h:270")
-    }
-
-
-    //}
 })
